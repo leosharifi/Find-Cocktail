669b375cb4f862b0886c4f24428495db81d9820d
Leo Nexus <elyassharifi2548@gmail.com>
2025-09-09 23:27:45 +0430

    first move with api

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(e) {
+    e.preventDefault();
+    
+    let input = document.querySelector('input').value;
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input}`)
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        if(data.drinks === null) {
+            document.getElementById('cocktailDisplay').innerHTML = '<h3>Try Another Search!</h3>';
+            return;
+        }
+        document.getElementById('cocktailDisplay').innerHTML = `
+            <h3>${data.drinks[0].strDrink}</h3>
+            <p>${data.drinks[0].strInstructions}</p>
+            <img src="${data.drinks[0].strDrinkThumb}" alt="">
+        `;
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
